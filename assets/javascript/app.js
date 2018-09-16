69135b258fa235e4d8deb5c2542bf3372b2dd9ce
Perri Specter <plspecter@gmail.com>
2018-09-15 20:11:47 -0400

    Train-Activity-Basic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,18 @@
 console.log("works");
 
-$(document).ready (function() {
+$(document).ready(function () {
 
+    var config = {
+        apiKey: "AIzaSyCju_h7JQYuDoZ8cK2lkFJvrJxuRi8n-Ds",
+        authDomain: "train-activity-basic-e40ab.firebaseapp.com",
+        databaseURL: "https://train-activity-basic-e40ab.firebaseio.com",
+        projectId: "train-activity-basic-e40ab",
+        storageBucket: "",
+        messagingSenderId: "873092454034"
+    };
+    firebase.initializeApp(config);
 
+    
 
 
 });
